4d0af404e973bae74c065596a67b19de07717113
Byran Wills-Heath <byran@adgico.co.uk>
2019-10-31 19:15:13 +0000

    Add SerialPort class

---
+++ DecoderDemo/SerialPort.h
@@ -0,0 +1,17 @@
+#ifndef CARINABUSDEBUGGER_SERIALPORT_H
+#define CARINABUSDEBUGGER_SERIALPORT_H
+
+#include <cstddef>
+
+class SerialPort
+{
+private:
+	int fd{-1};
+public:
+	SerialPort(char const* portname);
+
+	void Write(void const* buffer, size_t length);
+	size_t Read(void* buffer, size_t length);
+};
+
+#endif //CARINABUSDEBUGGER_SERIALPORT_H
