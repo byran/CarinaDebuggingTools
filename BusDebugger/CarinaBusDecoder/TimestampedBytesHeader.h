a14be30469013351ea24a79f7d25aca0be506853
Byran Wills-Heath <byran@adgico.co.uk>
2020-08-06 11:51:55 +0100

    Make CarinaBusDecoder run on Windows

--- BusDebugger/CarinaBusDecoder/TimestampedBytesHeader.h
+++ BusDebugger/CarinaBusDecoder/TimestampedBytesHeader.h
@@ -2,11 +2,21 @@
 #define CARINABUSDEBUGGER_TIMESTAMPEDBYTESHEADER_H
 
 #include <cstdint>
+#include "CompilerSupport.h"
+
+#ifdef WIN32
+#pragma pack(1)
+#endif
 
 struct TimestampedBytesHeader
 {
 	uint32_t time;
 	uint32_t length;
 } __attribute__((packed));
+
+#ifdef WIN32
+#pragma pack()
+#endif
+
 
 #endif  // CARINABUSDEBUGGER_TIMESTAMPEDBYTESHEADER_H
