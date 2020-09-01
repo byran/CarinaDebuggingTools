5014dbe5f84ebe247f1ca8e5ffba24af816dbde1
Byran Wills-Heath <byran@adgico.co.uk>
2020-09-01 16:46:56 +0100

    Tidy up REcordedLogFileWriter.cpp

--- BusDebugger/CarinaBusDecoder/RecordedLogFileWriter.cpp
+++ BusDebugger/CarinaBusDecoder/RecordedLogFileWriter.cpp
@@ -4,18 +4,18 @@
 
 #include <cstdint>
 
+using namespace ::std::chrono;
+
 RecordedLogFileWriter::RecordedLogFileWriter(char const* name)
 	: file(name, std::ios_base::binary)
 {
-	startTime = ::std::chrono::steady_clock::now();
+	startTime = steady_clock::now();
 }
-
-using namespace ::std::chrono;
 
 void RecordedLogFileWriter::Write(void const* buffer, size_t length)
 {
-	auto now = ::std::chrono::steady_clock::now();
-	auto delta = duration_cast<std::chrono::milliseconds>(now - startTime);
+	auto now = steady_clock::now();
+	auto delta = duration_cast<milliseconds>(now - startTime);
 	TimestampedBytesHeader header;
 	header.time = delta.count();
 	header.length = length;
